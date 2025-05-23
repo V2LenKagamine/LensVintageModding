2dcacea2d06931f9420b18f35471330f5254d483
Len Kagamine <xboxmaster2000@hotmail.com>
2025-05-23 14:58:05 -0400

    Update Progress
    yippe

--- LensClasses/lensclasses/LensClassesModSystem.cs
+++ LensClasses/lensclasses/LensClassesModSystem.cs
@@ -4,29 +4,18 @@ using Vintagestory.API.Server;
 
 namespace LensstoryMod
 {
-    public class LensClassesMod : ModSystem
+    public class LensCommonsMod : ModSystem
     {
         public static ILogger logger;
         public override void Start(ICoreAPI api)
         {
             base.Start(api);
-            api.RegisterItemClass("scrollitem", typeof(ScrollItem));
+            api.RegisterItemClass("lenstempgiftitem", typeof(TemporalGiftItem));
             logger = api.Logger;
         }
         internal static void LogError(string message)
         {
-            logger?.Error("(LensStory):{0}", message);
-        }
-        public override void StartServerSide(ICoreServerAPI api)
-        {
-            api.Event.PlayerNowPlaying += (IServerPlayer player) =>
-            {
-                if (player.Entity is EntityPlayer)
-                {
-                    Entity e = player.Entity;
-                    e.AddBehavior(new ScrollStuffBhv(e));
-                }
-            };
+            logger?.Error("(LensClasses):{0}", message);
         }
     }
 }
---
+++ LensClasses/lensclasses/src/temporalgift.cs
@@ -0,0 +1,86 @@
+﻿using System.Text;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Datastructures;
+
+namespace LensstoryMod
+{
+    public class TemporalGiftItem : Item
+    {
+        public override void OnHeldUseStart(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, EnumHandInteract useType, bool firstEvent, ref EnumHandHandling handling)
+        {
+            handling = EnumHandHandling.Handled;
+            if(api.World.Side == EnumAppSide.Server)
+            {
+                if (!slot.Itemstack.Attributes.HasAttribute("gifts")) { return; }
+                var itemattr = ((TreeArrayAttribute)slot.Itemstack.Attributes.GetTreeAttribute("gifts")["itemlist"])?.value;
+                foreach (var thing in itemattr)
+                {
+
+                    var code = thing.GetString("type");
+                    ItemStack yep;
+                    switch (code)
+                    {
+                        case string x when x == "block" || x == "Block":
+                            {
+                                yep = new(api.World.GetBlock(new AssetLocation(thing.GetString("code"))), thing.GetAsInt("amount", 1));
+                                break;
+                            }
+                        case string x when x == "item" || x == "Item":
+                            {
+                                yep = new(api.World.GetItem(new AssetLocation(thing.GetString("code"))), thing.GetAsInt("amount", 1));
+                                break;
+                            }
+
+                        default:
+                            { continue; }
+                    }
+                    var didgive = byEntity.TryGiveItemStack(yep);
+                    if(!didgive)
+                    {
+                        api.World.SpawnItemEntity(yep,byEntity.Pos.Copy().Add(0.5f,0.5f,0.5f).AsBlockPos.ToVec3d());
+                    }
+                }
+                slot.TakeOutWhole();
+                slot.MarkDirty();
+            }
+        }
+
+        public override void GetHeldItemInfo(ItemSlot slot, StringBuilder dsc, IWorldAccessor world, bool withDebugInfo)
+        {
+            base.GetHeldItemInfo(slot, dsc, world, withDebugInfo);
+
+            dsc.Append("\n");
+            dsc.AppendLine("You feel it contains the following:");
+
+            if (!slot.Itemstack.Attributes.HasAttribute("gifts")) { return; }
+            var itemattr = ((TreeArrayAttribute)slot.Itemstack.Attributes.GetTreeAttribute("gifts")["itemlist"])?.value;
+            foreach (var thing in itemattr)
+            {
+
+                var code = thing.GetString("type");
+                ItemStack yep;
+                switch (code)
+                {
+                    case string x when x == "block" || x == "Block":
+                        {
+                            yep = new(api.World.GetBlock(new AssetLocation(thing.GetString("code"))), thing.GetAsInt("amount", 1));
+                            break;
+                        }
+                    case string x when x == "item" || x == "Item":
+                        {
+                            yep = new(api.World.GetItem(new AssetLocation(thing.GetString("code"))), thing.GetAsInt("amount", 1));
+                            break;
+                        }
+
+                    default:
+                        { continue; }
+                }
+
+                if (yep != null) {
+                    dsc.AppendLine(yep.GetName() + " x" + yep.StackSize);
+                }
+            }
+        }
+    }
+}
