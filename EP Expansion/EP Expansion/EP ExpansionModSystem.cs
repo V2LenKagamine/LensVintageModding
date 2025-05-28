beb593490f83f737257386cbafba53d1d3fa2226
Len Kagamine <xboxmaster2000@hotmail.com>
2025-05-28 19:48:27 -0400

    adaawaw

---
+++ EP Expansion/EP Expansion/EP ExpansionModSystem.cs
@@ -0,0 +1,27 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace EP_Expansion
+{
+    public class EP_ExpansionModSystem : ModSystem
+    {
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+            api.Logger.Notification("Hello from template mod: " + api.Side);
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            api.Logger.Notification("Hello from template mod server side: " + Lang.Get("EP_Expansion:hello"));
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            api.Logger.Notification("Hello from template mod client side: " + Lang.Get("EP_Expansion:hello"));
+        }
+    }
+}
