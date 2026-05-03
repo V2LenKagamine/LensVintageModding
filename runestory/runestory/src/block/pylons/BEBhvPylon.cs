ecdf96ed5f5213fddacbe087bb51e21bd94de33e
Len Kagamine <xboxmaster2000@hotmail.com>
2026-05-03 00:24:27 -0400

    IT LOADS

--- runestory/runestory/src/block/pylons/BEBhvPylon.cs
+++ runestory/runestory/src/block/pylons/BEBhvPylon.cs
@@ -1,39 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Datastructures;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Util;
+using Vintagestory.Client.NoObf;
 
 namespace runestory.src.block.pylons
 {
-    public class BEBhvPylon : BlockEntityBehavior
+    public class BEBhvPylon : BlockEntityBehavior,ITexPositionSource
     {
 
         PylonRenderer render;
 
-        MeshData mesh
+       private MeshData mesh(ITesselatorAPI tess)
         {
-            get
+            Dictionary<string,MeshData> blockMeshes = ObjectCacheUtil.GetOrCreate(Api, "runepylonMeshes", () => new Dictionary<string, MeshData>());
+            if (blockMeshes.TryGetValue("runepylonmeshcode", out var mesh)) return mesh;
+            return blockMeshes["runepylonmeshcode"] = GenMesh((Api as ICoreClientAPI).BlockTextureAtlas);
+        }
+        public ITextureAtlasAPI targetAtlas;
+        public Size2i AtlasSize => targetAtlas.Size;
+
+        public TextureAtlasPosition this[string textureCode] => GetOrCreateTexPos(tmpTextures[textureCode]);
+        public readonly Dictionary<string, AssetLocation> tmpTextures = new();
+
+        protected TextureAtlasPosition GetOrCreateTexPos(AssetLocation texturePath)
+        {
+            ICoreClientAPI capi = Api as ICoreClientAPI;
+            IAsset texAsset = capi.Assets.TryGet(texturePath.Clone().WithPathPrefixOnce("textures/blocks/").WithPathAppendixOnce(".png"));
+            TextureAtlasPosition texPos = targetAtlas[texturePath];
+
+            if (texPos != null)
             {
-                object val;
-                Api.ObjectCache.TryGetValue("pylon-basemesh", out val);
-                return (MeshData)val;
+                return texPos;
             }
-            set
+
+            if (texAsset != null)
             {
-                Api.ObjectCache["pylon-basemesh"] = value;
+                capi.Event.EnqueueMainThreadTask(() => targetAtlas.GetOrInsertTexture(texturePath, out int _, out texPos, () =>texAsset.ToBitmap(capi)), "");
             }
+
+            return texPos ?? capi.BlockTextureAtlas.UnknownTexturePosition;
         }
 
-        internal MeshData GenMesh()
+        internal MeshData GenMesh(ITextureAtlasAPI tart)
         {
             Block block = Api.World.BlockAccessor.GetBlock(Pos);
             if (block.Id == 0) return null;
             MeshData mesh;
             ITesselatorAPI mesman = ((ICoreClientAPI)Api).Tesselator;
 
-            mesman.TesselateShape(block, Api.Assets.TryGet("runestory:shapes/blocks/runepylon.json").ToObject<Shape>(), out mesh);
+            targetAtlas = tart;
+
+            foreach (KeyValuePair<string, CompositeTexture> key in Block.Textures)
+            {
+                tmpTextures[key.Key] = Block.Textures.FirstOrDefault().Value.Base;
+            }
+
+            CompositeShape shape = block.Attributes["shape"].AsObject<CompositeShape>();
+            LenUtil.TessellateObj(mesman as ShapeTesselator, shape, out mesh, this["obj"],Api as ICoreClientAPI,"runestory:shapes/blocks/runepylon.obj");
+            //mesman.TesselateShape(block, Api.Assets.TryGet("runestory:shapes/blocks/runepylon.json").ToObject<Shape>(), out mesh);
 
             return mesh;
         }
@@ -47,20 +77,15 @@ namespace runestory.src.block.pylons
             base.Initialize(api, properties);
 
             if(api.Side != EnumAppSide.Client) { return; }
-            render = new PylonRenderer(api as ICoreClientAPI,Pos,GenMesh());
+            render = new PylonRenderer(api as ICoreClientAPI,Pos,GenMesh((api as ICoreClientAPI).BlockTextureAtlas));
 
             (api as ICoreClientAPI).Event.RegisterRenderer(render, EnumRenderStage.Opaque, "RunePylon");
-
-            if(mesh is null)
-            {
-                mesh = GenMesh();
-            }
         }
 
         public override bool OnTesselation(ITerrainMeshPool mesher, ITesselatorAPI tessThreadTesselator)
         {
             if(Block == null) return false;
-            mesher.AddMeshData(mesh);
+            mesher.AddMeshData(mesh(tessThreadTesselator));
             return true;
         }
 
@@ -68,7 +93,7 @@ namespace runestory.src.block.pylons
         {
             if (Api.Side == EnumAppSide.Client)
             {
-                mesh.Dispose();
+                mesh((Api as ICoreClientAPI).Tesselator).Dispose();
                 render.Dispose();
             }
             base.OnBlockRemoved();
---
+++ runestory/runestory/src/util/randomutil.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Datastructures;
+using Vintagestory.API.MathTools;
+using Vintagestory.Client.NoObf;
+using Vintagestory.Common;
+
+namespace runestory
+{
+    public static class LenUtil
+    {
+        public static void TakeKBFrom(ICoreAPI api, Entity from, Entity target, float strength)
+        {
+            if (target is null || from is null) { return; }
+            float exx = (float)(Math.Abs(from.Pos.X) - Math.Abs(target.Pos.X));
+            float why = (float)(Math.Abs(from.Pos.Y) - Math.Abs(target.Pos.Y));
+            float zee = (float)(Math.Abs(from.Pos.Z) - Math.Abs(target.Pos.Z));
+
+            Vec3d normed = new(exx, why, zee);
+
+            normed.Normalize();
+
+            normed.Y *= 0.5f;
+
+            float num = GameMath.Clamp((1f - target.Properties.KnockbackResistance) / 10f, 0f, 1f) * strength;
+
+
+            target.OnGround = false;
+            target.Pos.Y += 0.6f;
+            api.World.RegisterCallback((dt) =>
+            target.Pos.Motion += normed * -num, 100);
+        }
+
+        public static void TessellateObj(this ShapeTesselator tessellator, CompositeShape compositeShape, out MeshData modeldata, TextureAtlasPosition pos,ICoreClientAPI api,string backupasset)
+        {
+            var meta = tessellator.GetField<TesselationMetaData>("meta");
+            var objTesselator = tessellator.GetField<ObjTesselator>("objTesselator");
+            var objs = tessellator.GetField<Vintagestory.API.Datastructures.OrderedDictionary<AssetLocation, IAsset>>("objs");
+
+            meta.UsesColorMap = false;
+            if (objs.TryGetValue(compositeShape.Base) is null)
+            {
+                objs.Add(compositeShape.Base, api.Assets.TryGet(backupasset));
+            }
+            objTesselator.Load(objs[compositeShape.Base],out modeldata,pos,meta,0);
+            tessellator.ApplyCompositeShapeModifiers(ref modeldata, compositeShape);
+        }
+        public static T GetField<T>(this object instance, string fieldName)
+        {
+            return (T)AccessTools.Field(instance.GetType(), fieldName).GetValue(instance);
+        }
+
+        public static void SetField(this object instance, string fieldName,object value)
+        {
+           AccessTools.Field(instance.GetType(), fieldName).SetValue(instance,value);
+        }
+
+        public static IAsset GetOrCreateObj(ICoreClientAPI capi,AssetLocation objpath)
+        {
+            IAsset texAsset = capi.Assets.TryGet(objpath.Clone().WithPathPrefixOnce("textures/").WithPathAppendixOnce(".obj"));
+
+            if(texAsset is null)
+            {
+                capi.Event.EnqueueMainThreadTask(() => capi.Assets.AddModOrigin("runestory", objpath), "");
+                texAsset = capi.Assets.TryGet(objpath.Clone().WithPathPrefixOnce("textures/").WithPathAppendixOnce(".obj"));
+            }
+
+            return texAsset;
+        }
+    }
+}
