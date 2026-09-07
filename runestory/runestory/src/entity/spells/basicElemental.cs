fb63676e6fdbe891ec368251210a4da554206915
Len Kagamine <xboxmaster2000@hotmail.com>
2026-09-07 04:04:14 -0400

    Tiny Lang Change + Wortor
    Also water heals now

--- runestory/runestory/src/entity/spells/basicElemental.cs
+++ runestory/runestory/src/entity/spells/basicElemental.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
 using Vintagestory.API.MathTools;
+using Vintagestory.Client.NoObf;
 using Vintagestory.GameContent;
 
 namespace runestory.src.entity.spells
@@ -34,9 +35,9 @@ namespace runestory.src.entity.spells
         public void HitEntity(Entity entity)
         {
             if (Api.Side == EnumAppSide.Client || entity is null) { return; }
-            int tier = ourSpell.spellTier;
+            int? tier = ourSpell.spellTier;
             Vec2f aoe = new(0f, 0f);
-            if (tier == 0) { return; }
+            if (tier is null || tier == 0) { return; }
             float dam = 1f;
             switch (tier)
             {
@@ -74,7 +75,7 @@ namespace runestory.src.entity.spells
                 Source = EnumDamageSource.Player,
                 CauseEntity = spawnedBy,
                 SourceEntity = this,
-                KnockbackStrength = 0.75f * tier,
+                KnockbackStrength = 0.75f * tier ?? 1,
                 Type = EnumDamageType.PiercingAttack
 
             };
@@ -82,7 +83,13 @@ namespace runestory.src.entity.spells
 
             if (ourSpell.ElementalType.Contains("water"))
             {
-                (spawnedBy as EntityPlayer).GetBehavior<PlayerTempBuffer>()?.AddTempBuff(spawnedBy as EntityPlayer, RunestoryMS.RMS_Stat_MagicDamage, 0.05f * tier, (30 * 1000) * tier, "waterbuff");
+                spawnedBy.ReceiveDamage(new DamageSource()
+                {
+                    Source = EnumDamageSource.Unknown,
+                    Type = EnumDamageType.Heal,
+                    TicksPerDuration = 1,
+                    Duration = TimeSpan.FromSeconds(0.5f * tier ?? 1)
+                }, 0.25f * tier ?? 1);
             }
             if (ourSpell.ElementalType.Contains("earth"))
             {
