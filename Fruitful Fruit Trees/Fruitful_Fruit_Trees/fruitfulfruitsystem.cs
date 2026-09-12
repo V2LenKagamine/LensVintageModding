9850a55cc63f5eabc7bb3a6504a9e01bc381a759
Len Kagamine <xboxmaster2000@hotmail.com>
2026-09-12 17:13:03 -0400

    Fix ore crymtal glass,remove tree
    theres a better mod anyway.

--- Fruitful Fruit Trees/Fruitful_Fruit_Trees/fruitfulfruitsystem.cs
+++
@@ -1,8 +0,0 @@
-﻿using Vintagestory.API.Common;
-
-namespace Fruitful_Fruit_Trees
-{
-    public class FruitfulfruitSystem : ModSystem
-    {
-    }
-}
--- Fruitful Fruit Trees/build.ps1
+++
@@ -1,3 +0,0 @@
-dotnet run --project CakeBuild/CakeBuild.csproj -- $args
-pause
-exit $LASTEXITCODE;
